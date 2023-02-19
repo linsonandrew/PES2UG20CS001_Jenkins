9729d25f91d58cf218af57b996aec088ef7c3608
A.Linson <79594175+linsonandrew@users.noreply.github.com>
2023-02-19 23:17:57 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
- cout<<"My name is A.Linson PES2UG20CS001";
+ cout<<"My name is A.Linson PES2UG20CS001"<<endl;
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
